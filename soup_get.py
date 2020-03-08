ee98d0c8b69811257ce5681ff0a1aa188e664305
sanshungit <39847510+sanshungit@users.noreply.github.com>
2020-03-08 20:53:48 +0800

    Add files via upload

---
+++ soup_get.py
@@ -0,0 +1,27 @@
+from time import time
+import requests
+
+            
+class Downloadhandler(object):
+
+    def __init__(self,url):
+        self.url=url
+
+    def refresh_load(self):
+        resp=requests.get(self.url)
+        data_model=resp.json()
+        return data_model['newslist'][0]['content']
+    
+def main():
+    url='http://api.tianapi.com/txapi/dujitang/index?key=89ab0002d0c5031afb9fb88d88dd33ef'
+    f=open('./毒鸡汤.txt','a+')
+
+    print('开始抓取....')
+    for _ in range(1000):
+       soup_str= Downloadhandler(url).refresh_load()
+       f.write(soup_str+'\n')
+    f.close()
+    print('抓取完成！！！')
+
+if __name__=='__main__':
+    main()
